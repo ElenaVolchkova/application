6ae1d8ae0ac6e74b133bcca261bea116081508d2
ElenaVolchkova <sombradzi@gmail.com>
2020-03-05 00:10:07 +0300

    Added Bootstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    # 'bootstrap4',
     'mysite.titova',
     'mysite.athletes',
     'mysite.amazons',
